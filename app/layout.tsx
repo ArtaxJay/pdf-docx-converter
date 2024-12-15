e4ce70be13f211d397da6bb9bd92e69c0dae644b
ArtaxJay <okjaceauthor@gmail.com>
2024-12-15 13:13:25 +0100

    Fix: hydrating error resolved

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html
+      suppressHydrationWarning
+      lang='en'
+    >
       {/*
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
