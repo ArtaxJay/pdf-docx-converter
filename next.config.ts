39e72a2e6c6de14f5ee5e967fade95723b2d8e1a
ArtaxJay <okjaceauthor@gmail.com>
2024-12-29 21:59:55 +0100

    Feature: add proxy to next.config.ts to allow direct use of localhost:

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  exports: {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://localhost:5000/:path*', // Proxy to Backend
+        },
+      ];
+    },
+  },
 };
 
 export default nextConfig;
